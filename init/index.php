2bef2db8d2deb789fb09d98d900cd20192d4c4c3
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-12-08 21:40:29 -0300

    fix: remove tests of connection

--- init/index.php
+++ init/index.php
@@ -2,18 +2,5 @@
 
 include __DIR__ . '/../router/routes.php';
 include __DIR__ . '/../config/env.php';
-include __DIR__ . '/../db/DB.php';
 
 echo $_SERVER['REQUEST_URI'];
-
-$db = DB::connect(
-    $_ENV['DB_HOST'],
-    $_ENV['DB_NAME'],
-    $_ENV['DB_USER'],
-    $_ENV['DB_PASSWORD']
-);
-
-if($db !== null)
-{
-    var_dump($db->getConnection());
-}
