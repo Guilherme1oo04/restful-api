ba19ee2effafcd053c6e71010315d468fb941adc
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-12-22 19:03:04 -0300

    feat: create function to insert data in database

--- db/DB.php
+++ db/DB.php
@@ -1,7 +1,10 @@
 <?php
 
-include __DIR__ . '/../utils/log.php';
+include __DIR__ . '/../utils/Log.php';
 
+/**
+ * Classe de operações com o banco de dados
+ */
 class DB
 {
 	protected $connection;
@@ -20,7 +23,7 @@ class DB
 		}
 		catch(PDOException $e)
 		{
-			writeLog("Connection failed: " . $e->getMessage());
+			Log::write("Connection failed: " . $e->getMessage());
 			return null;
 		}
 
@@ -34,7 +37,7 @@ class DB
 
 		if(!file_exists($sqlTablesPath))
 		{
-			writeLog("File not found: $stringPath");
+			Log::write("File not found: $stringPath");
 			return false;
 		}
 
@@ -42,7 +45,7 @@ class DB
 
 		if($this->connection === null)
 		{
-			writeLog("PDO Connection is invalid");
+			Log::write("Error creating tables: PDO Connection is invalid");
 			return false;
 		}
 
@@ -52,7 +55,49 @@ class DB
 		}
 		catch(PDOException $e)
 		{
-			writeLog("Error creating tables: " . $e->getMessage());
+			Log::write("Error creating tables: " . $e->getMessage());
+			return false;
+		}
+
+		return true;
+	}
+
+	public function insert(string $table, array $data): bool
+	{
+		if($this->connection === null)
+		{
+			Log::write("Error inserting data: PDO Connection is invalid");
+			return false;
+		}
+
+		if(empty($data))
+		{
+			Log::write("Error inserting data: Data is empty");
+			return false;
+		}
+
+		$this->connection->beginTransaction();
+
+		try
+		{
+			$colunms = array_keys($data);
+
+			$sql = "INSERT INTO $table (";
+			$sql .= implode(", ", $colunms);
+			$sql .= ") VALUES (";
+			$sql .= implode(", ", array_fill(0, count($colunms), "?"));
+			$sql .= ")";
+
+			$statement = $this->connection->prepare($sql);
+
+			$statement->execute(array_values($data));
+
+			$this->connection->commit();
+		}
+		catch(PDOException $e)
+		{
+			$this->connection->rollBack();
+			Log::write("Error inserting data: " . $e->getMessage());
 			return false;
 		}
 
