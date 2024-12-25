145e4205a861519dbae6c9e944dcb23635700e5a
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-12-25 11:26:32 -0300

    feat: create recursive function to get route

--- router/routes.php
+++ router/routes.php
@@ -3,9 +3,27 @@
 include __DIR__ . '/../classes/Route.php';
 include __DIR__ . '/../classes/RouteGroup.php';
 
+function getSelectedRoute(Route | RouteGroup | null $route, array $path): Route | RouteGroup | null
+{
+	if($route instanceof RouteGroup)
+	{
+		if(empty($path))
+		{
+			return getSelectedRoute($route->getRoute("/"), []);
+		}
+
+		return getSelectedRoute($route->getRoute("/" . $path[0]), array_slice($path, 1));
+	}
+
+	return $route;
+}
+
 $routes = [
-	'/users' => new RouteGroup('/users', [
-		new Route('/', 'GET', 'user/GetUsers', 'GetUsers'),
-		// new Route('/{id}', 'GET', ),
-	])
+	new RouteGroup('/api', [
+		new RouteGroup('/users', [
+			new Route('/', 'GET', 'user/GetUsers', 'GetUsers'),
+			// new Route('/{id}', 'GET', ),
+		]),
+		new Route('/teste', 'GET', 'user/GetUsers', 'GetUsers'),
+	]),
 ];
