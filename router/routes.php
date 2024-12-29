820e4579af9e94df9729ea6c1b256295036993d9
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-12-29 20:05:56 -0300

    feat: update recursive function to fix bugs

--- router/routes.php
+++ router/routes.php
@@ -3,16 +3,21 @@
 include __DIR__ . '/../classes/Route.php';
 include __DIR__ . '/../classes/RouteGroup.php';
 
-function getSelectedRoute(Route | RouteGroup | null $route, array $path): Route | RouteGroup | null
+function getSelectedRoute(Route | RouteGroup | null $route, array $path, string $requestMethod): Route | RouteGroup | null
 {
 	if($route instanceof RouteGroup)
 	{
 		if(empty($path))
 		{
-			return getSelectedRoute($route->getRoute("/"), []);
+			return getSelectedRoute($route->getRoute("/", $requestMethod), [], $requestMethod);
 		}
 
-		return getSelectedRoute($route->getRoute("/" . $path[0]), array_slice($path, 1));
+		return getSelectedRoute($route->getRoute("/" . $path[0], $requestMethod), array_slice($path, 1), $requestMethod);
+	}
+
+	if(count($path) > 0)
+	{
+		return null;
 	}
 
 	return $route;
@@ -22,7 +27,7 @@ $routes = [
 	new RouteGroup('/api', [
 		new RouteGroup('/users', [
 			new Route('/', 'GET', 'user/GetUsers', 'GetUsers'),
-			// new Route('/{id}', 'GET', ),
+			new Route('/{id}', 'GET', 'user/GetUserById', 'GetUserById'),
 		]),
 		new Route('/teste', 'GET', 'user/GetUsers', 'GetUsers'),
 	]),
