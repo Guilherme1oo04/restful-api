6010710df9cf8ccc0bf6e114d72028ec622d6c61
Guilherme Chaves <guilherme.9999.chaves@gmail.com>
2024-12-29 20:05:22 -0300

    feat: update get users controller

--- controllers/user/GetUsers.php
+++ controllers/user/GetUsers.php
@@ -4,19 +4,25 @@ include __DIR__ . '/../../classes/Models/User.php';
 
 class GetUsers
 {
-	public function __invoke(DB $db): array
+	public function __invoke(DB $db, array $queryParams): array
 	{
 		$users = User::all($db);
 
 		if(empty($users))
 		{
-			return [];
+			return [
+				"statusCode" => 200,
+				"data" => []
+			];
 		}
 
 		$users = array_map(function($user) {
 			return $user->toArray();
 		}, $users);
 
-		return $users;
+		return [
+			"statusCode" => 200,
+			"data" => $users
+		];
 	}
 }
